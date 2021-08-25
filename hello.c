bddd0454299d877e70325596615d8d5622a4cd1b
MrE42 <eschoneweis2@unl.edu>
2021-08-25 20:48:18 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Elijah Schoneweis
+ *
+ * Date: 2021/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Elijah Schoneweis!!!\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
